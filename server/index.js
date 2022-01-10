cc4fe79989887a8c5fe2b0ccdde9dc32f1e1e760
Justin Delille <justind7513@gmail.com>
2022-01-10 16:40:08 -0700

    remove cache

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser")
+
+dotenv.config();
+
+// set up express server
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.listen(PORT, () => console.log(`Server started from port ${PORT}`));
+
+// set up routers
+
+app.use("/snippet", require("./routers/snippetRouter"));
+app.use("/auth", require("./routers/userRouter"));
+
+// connect to mongodb
+
+mongoose.connect(
+  process.env.MDB_CONNECT_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
